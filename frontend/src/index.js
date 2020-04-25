0e373bb886a57e9a7f6f9d81b072108ce6e60e37
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-25 18:33:42 -0400

    Added marterialize

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/custom.scss';
+import 'materialize-css/dist/css/materialize.min.css'
 
 ReactDOM.render(
   <React.StrictMode>

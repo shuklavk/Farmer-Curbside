595c53517311d029f962c7b2beae0e1274b42677
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-26 06:58:10 -0400

    Adjusted UI on the fulfilment card

--- frontend/src/components/FulFillmentCard.js
+++ frontend/src/components/FulFillmentCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,11 +9,12 @@ import Header from './FarmerHeader';
 import '../styles/FulFillmentCard.css'
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
 import {
   withStyles,
 } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
   root: {
     flexGrow: 1,
     padding: '40px',
@@ -24,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
     fontSize: '35px',
   },
-}));
+});
 
 const ColorButton = withStyles(theme => ({
   root: {
@@ -41,34 +42,61 @@ const testArr = [{productName:'oranges', soldQuantity:10, parkingSpot:'Z54', del
 ];
 // ***************************************************
 
-export default function FulFillmentCard() {
-  const classes = useStyles();
-  let ordersArr = testArr.map((order) => {
+class FulFillmentCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      purchases: [],
+      dataFetched: false
+    }
+  }
+
+  fetchItems() {
+    const { user } = this.props;
+    axios.get(`/api/showAll/purchasesReady/${user._id}`)
+    .then((res) => {
+        console.log(res);
+        this.setState({ dataFetched: true });
+    })
+    .catch((err) => {
+        console.log(err);
+        this.setState({ dataFetched: true });
+    })
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { purchases, dataFetched } = this.state;
+    const { user } = this.props;
+    if (user && !dataFetched) {
+      this.fetchItems();
+    }
+    
     return (
-      <Grid item xs={12}>
-        <Paper justify="space-evenly" className={classes.paper}>Deliver {order.soldQuantity} {order.productName} to parking spot #{order.parkingSpot}      
-        <span className='codeSpan'> <span className= 'code'>CODE: {order.deliveryCode}</span>
-        <ColorButton className = {classes.button} variant="contained" size='large'>Fulfill Order</ColorButton>
-        </span>
-        </Paper>
-      </Grid>
-    )
-  })
-  
-  return (
-    <div>
-      <Header />
-      <div className={classes.root}>
-        <Grid container spacing={3} 
-          // direction="column"
-          justify="space-evenly"
-          alignItems="center"
-        >
-          {ordersArr}
-        </Grid>
+      <div>
+        <Header />
+        <div className={classes.root}>
+          <Grid container spacing={3} 
+            // direction="column"
+            justify="space-evenly"
+            alignItems="center"
+          >
+            {testArr.map((order) => {
+              return (
+                <Grid item xs={12}>
+                  <Paper justify="space-evenly" className={classes.paper}>Deliver {order.soldQuantity} {order.productName} to parking spot #{order.parkingSpot}      
+                  <span className='codeSpan'> <span className= 'code'>CODE: {order.deliveryCode}</span>
+                  <ColorButton className = {classes.button} variant="contained" size='large'>Fulfill Order</ColorButton>
+                  </span>
+                  </Paper>
+                </Grid>
+              )
+            })}
+          </Grid>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-// export default SimpleCard;
+export default withStyles(styles)(FulFillmentCard);

62c937774423ffb3e9455cafa7f06ecb8dbb22a2
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-26 03:08:56 -0400

    Began connecting view items page on customer side to backend

--- frontend/src/components/CustomerPurchase.js
+++ frontend/src/components/CustomerPurchase.js
@@ -1,52 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FarmerCard from './FarmerCard';
 import Header from './CustomerHeader';
+import axios from 'axios';
 import '../styles/CustomerPurchase.css'
 
-function CustomerPurchase() {
-    let farmerData = [
-      {
-        name: "David",
-        product: "Apples",
-        price: 5.99,
-      },
-      {
-        name: "Joe",
-        product: "Tomatoes",
-        price: 4.99,
-      },{
-        name: "Joe",
-        product: "Pears",
-        price: 4.99,
-      },{
-        name: "Joe",
-        product: "Grapes",
-        price: 4.99,
-      },
-      {
-        name: "Joe",
-        product: "Bananas",
-        price: 4.99,
-      },
-      {
-        name: "Joe",
-        product: "Bananas",
-        price: 4.99,
-      },
-      {
-        name: "Joe",
-        product: "Bananas",
-        price: 4.99,
-      },
-    ];
-    return (
-      <div>
-        <Header />
-        <div className="CustomerPurchase">
-          {farmerData.map((farmer) => (<FarmerCard name={farmer.name} product={farmer.product} price={farmer.price}/>))}
+class CustomerPurchase extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        farmerData: [],
+        dataFetched: false
+      }
+    }
+  
+    fetchItems() {
+      const { user } = this.props;
+      if (user) {
+        axios.get(`/api/showAll/items`)
+        .then((res) => {
+          console.log(res)
+          this.setState({ dataFetched: true, productData: res.data.results[0].items })
+        })
+        .catch((err) => {
+          console.log(err)
+          this.setState({ dataFetched: true })
+        })
+      }
+    }
+  
+    render() {
+      const { farmerData, dataFetched } = this.state;
+      const { user } = this.props;
+      if (user && !dataFetched) {
+        this.fetchItems();
+      }
+      return (
+        <div>
+          <Header />
+          <div className="CustomerPurchase">
+            {farmerData.map((farmer) => (<FarmerCard name={farmer.name} product={farmer.product} price={farmer.price}/>))}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
 }
   
 export default CustomerPurchase;
+
+    // let farmerData = [
+    //   {
+    //     name: "David",
+    //     product: "Apples",
+    //     price: 5.99,
+    //   },
+    //   {
+    //     name: "Joe",
+    //     product: "Tomatoes",
+    //     price: 4.99,
+    //   },{
+    //     name: "Joe",
+    //     product: "Pears",
+    //     price: 4.99,
+    //   },{
+    //     name: "Joe",
+    //     product: "Grapes",
+    //     price: 4.99,
+    //   },
+    //   {
+    //     name: "Joe",
+    //     product: "Bananas",
+    //     price: 4.99,
+    //   },
+    //   {
+    //     name: "Joe",
+    //     product: "Bananas",
+    //     price: 4.99,
+    //   },
+    //   {
+    //     name: "Joe",
+    //     product: "Bananas",
+    //     price: 4.99,
+    //   },
+    // ];

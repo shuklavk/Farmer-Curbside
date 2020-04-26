88418d851abe4b3867b42b5b1bba7b3a41c820ba
Vikas Shukla <shuk.vk@gmail.com>
2020-04-26 08:14:13 -0700

    finds location of closest farmers market geofence

---
+++ frontend/src/components/radarTrial.mjs
@@ -0,0 +1,5 @@
+import Radar from 'radar-sdk-js';
+Radar.initialize("prj_test_pk_01a1c84daec7742d8edf59f59fbdfb5c31e98cf3prj_test_pk_01a1c84daec7742d8edf59f59fbdfb5c31e98cf3");
+Radar.setUserId("5ea571a5d8e6e70404208b14");
+
+Radar.setDescription("test user");

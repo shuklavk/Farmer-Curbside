d20b214153006e025506186ff793d2baed5e830f
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-26 01:33:35 -0400

    Began implementing code on pickup confirmation

--- frontend/src/components/PickupConfirmation.js
+++ frontend/src/components/PickupConfirmation.js
@@ -3,16 +3,19 @@ import '../styles/PickupConfirmation.css'
 import image from "../assets/image3.png";
 import { Button } from '@material-ui/core'; 
 
-function PickupConfirmation() {
-    return (
+function PickupConfirmation(props) {
+  const { user } = props;
+  return (
+    <div>
       <div className="PickupConfirmation">
-        <h1>Your goods will be delivered shortly!<br></br>Your unique code for pickup is:<br></br> A3B6!</h1>
+        <h1>Your goods will be delivered shortly!<br></br>Your unique code for pickup is:<br></br> {user.code}!</h1>
         <form>
-            <Button variant="contained" color="secondary">Return To Discovery</Button>
+            <Button variant="contained" color="secondary" onClick={() => {this.window.location.href = '/customerpurchasex'}}>Return To Discovery</Button>
         </form>
         <img className="farmer-image"src={image}></img>
       </div>
-    );
+    </div>
+  );
 }
   
 export default PickupConfirmation;

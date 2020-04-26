857b82eb031aef2c70339f3a2117bbad0cf6fd26
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-26 01:40:59 -0400

    Finished making users code appear on pickup page

--- frontend/src/components/PickupConfirmation.js
+++ frontend/src/components/PickupConfirmation.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import '../styles/PickupConfirmation.css'
+import { Redirect } from 'react-router-dom';
+import '../styles/PickupConfirmation.css';
 import image from "../assets/image3.png";
+import Header from "../components/CustomerHeader";
 import { Button } from '@material-ui/core'; 
 
 function PickupConfirmation(props) {
-  const { user } = props;
+  const { user, loggedIn } = props;
+  console.log(user)
   return (
     <div>
-      <div className="PickupConfirmation">
-        <h1>Your goods will be delivered shortly!<br></br>Your unique code for pickup is:<br></br> {user.code}!</h1>
-        <form>
-            <Button variant="contained" color="secondary" onClick={() => {this.window.location.href = '/customerpurchasex'}}>Return To Discovery</Button>
-        </form>
-        <img className="farmer-image"src={image}></img>
-      </div>
+      {
+        loggedIn === false
+        ? (
+          <Redirect to="/login" />
+        )
+        : null
+      }
+      {
+        user !== null
+        ? (
+          <div className="PickupConfirmation">
+            <h1>Your goods will be delivered shortly!<br></br>Your unique code for pickup is:<br></br> {user.code}!</h1>
+            <form>
+                <Button variant="contained" color="secondary" onClick={() => {this.window.location.href = '/customerpurchasex'}}>Return To Discovery</Button>
+            </form>
+            <img className="farmer-image"src={image}></img>
+          </div>
+        )
+        : null
+      }
     </div>
   );
 }

99f30c04d87ab0f08ccaf6f03e9e13dc855fbe8c
Ashanth R <59122376+Ashxnth@users.noreply.github.com>
2020-04-25 22:43:41 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import ViewPurchase from './components/ViewPurchase';
 import CustomerPurchase from './components/CustomerPurchase';
 import FarmerAddItems from './components/FarmerAddItems';
 import Header from './components/Header';
+import CustomerPickup from './components/CustomerPickup';
+import PickupConfirmation from './components/PickupConfirmation';
 
 function App() {
   return (
@@ -26,6 +28,12 @@ function App() {
         </Route>
         <Route path="/customerpurchase">
           <CustomerPurchase />
+        </Route>
+        <Route path="/customerpickup">
+          <CustomerPickup />
+        </Route>
+        <Route path="/pickupconfirmation">
+          <PickupConfirmation />
         </Route>
         <Route path="/additems">
           <Header />
